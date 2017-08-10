34e916fff51d67fa7c1ec53e1baab9cef3881d0a
Wang Zuo <wzuoadjusted@gmail.com>
2017-08-10 22:46:58 +0800

    add graphql example

---
+++ examples/graphql.js
@@ -0,0 +1,69 @@
+import assert from 'assert';
+import {
+  graphql,
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLString
+} from 'graphql';
+import TinyRecord from '../src/TinyRecord';
+
+TinyRecord.Base.establishConnection({
+  adapter: 'sqlite3',
+  database: ':memory:'
+});
+
+const User = TinyRecord.createClass({
+  tableName: 'users'
+});
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: GraphQLInt },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString }
+  }
+});
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+      user: {
+        type: UserType,
+        args: { id: { type: GraphQLInt } },
+        async resolve(_, { id }) {
+          const user = await User.find(id);
+          return user;
+        }
+      }
+    }
+  })
+});
+
+(async function() {
+  await TinyRecord.Base.connection.createTable('users', { force: true }, t => {
+    t.string('name', 'email');
+    t.timestamps();
+  });
+
+  await User.create({ name: 'test', email: 'test@example.com' });
+
+  const result = await graphql(
+    schema,
+    `query {
+      user(id: 1) {
+        id name email
+      }
+    }`
+  );
+
+  console.log(result);
+
+  const { user } = result.data;
+
+  assert.equal(user.id, 1);
+  assert.equal(user.name, 'test');
+  assert.equal(user.email, 'test@example.com');
+})();
