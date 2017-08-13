4e329eb226b57522d1527c4801360da3c793e2e8
Wang Zuo <wzuoadjusted@gmail.com>
2017-08-13 12:34:37 +0800

    graphql resolve list of records

--- examples/graphql.test.js
+++ examples/graphql.test.js
@@ -3,6 +3,7 @@ import {
   GraphQLSchema,
   GraphQLObjectType,
   GraphQLInt,
+  GraphQLList,
   GraphQLString
 } from 'graphql';
 import TinyRecord from '../src/TinyRecord';
@@ -29,6 +30,13 @@ const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
+      allUsers: {
+        type: new GraphQLList(UserType),
+        async resolve() {
+          const users = await User.order('id').records();
+          return users;
+        }
+      },
       user: {
         type: UserType,
         args: { id: { type: GraphQLInt } },
@@ -41,26 +49,33 @@ const schema = new GraphQLSchema({
   })
 });
 
-test('resolve user', async () => {
+test('resolve', async () => {
   await TinyRecord.Base.connection.createTable('users', { force: true }, t => {
     t.string('name', 'email');
     t.timestamps();
   });
 
-  await User.create({ name: 'test', email: 'test@example.com' });
+  await User.create({ name: 'test1', email: 'test1@example.com' });
+  await User.create({ name: 'test2', email: 'test2@example.com' });
 
   const result = await graphql(
     schema,
     `query {
+      allUsers {
+        id
+      }
       user(id: 1) {
         id name email
       }
     }`
   );
 
-  const { user } = result.data;
+  const { user, allUsers: users } = result.data;
 
   expect(user.id).toBe(1);
-  expect(user.name).toBe('test');
-  expect(user.email).toBe('test@example.com');
+  expect(user.name).toBe('test1');
+  expect(user.email).toBe('test1@example.com');
+
+  expect(users.length).toBe(2);
+  expect(users.map(x => x.id)).toEqual([1, 2]);
 });
