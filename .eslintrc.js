128abf8aaa32c39ffd7562873587a7353781d65b
Wang Zuo <wzuoadjusted@gmail.com>
2018-07-14 12:24:26 +0800

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,19 +4,19 @@ module.exports = {
     commonjs: true,
     es6: true,
     node: true,
-    'jest/globals': true
+    jest: true
   },
   extends: ['eslint:recommended'],
   rules: {
-    'no-console': 'off',
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'always']
+    'no-console': 'off'
   },
   parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 6,
     sourceType: 'module'
   },
-  plugins: ['jest']
+  ecmaFeatures: {
+    generators: true,
+    experimentalObjectRestSpread: true
+  }
 };
